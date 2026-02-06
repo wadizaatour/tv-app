d47f5cbed76489b0f8335dfd371fe81c8b7bf72c
Your Name <you@example.com>
2026-02-06 18:47:13 +0100

    remove plugins to keep minimum setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import Home from '../pages/home.vue'
+import Details from '../pages/details.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/details',
+    name: 'Details',
+    component: Details,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import AutoImport from 'unplugin-auto-import/vite'
-import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    VueRouter({
-      dts: 'src/typed-router.d.ts',
-    }),
-    vue(),
-    AutoImport({
-      imports: ['vue', 'vue-router'],
-      dts: 'src/auto-imports.d.ts',
-    }),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
